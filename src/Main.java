0bb0fa32cb67bf31c1eeff5c2786f0e496dff47e
VictorGazarini <victorhugogazarini@gmail.com>
2025-02-27 22:05:03 -0300

    Atualização de verificação de senha

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Bem vindo ao login da sua conta bancaria!");
 
+        System.out.println("Digite seu nome completo: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Escreva sua senha: ");
+        String senha = scanner.nextLine();
+
+        String resultado = SenhaValidacao.validarSenha(senha);
+
+        System.out.println(resultado);
     }
 }
